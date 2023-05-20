053b5acf15632e25d3814404d0e734103ee74cd7
lee <wy_1407@outlook.com>
2023-05-20 13:41:46 +0800

    优化逻辑

--- App.js
+++ App.js
@@ -11,17 +11,43 @@ const App = () => {
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
 
-  const handlePress = (value) => {
-    setInput((prevInput) => prevInput + value);
-  };
+const handlePress = (value) => {
+  setInput((prevInput) => {
+    // 检查是否连续输入运算符
+    if (/[+\-*/.]$/.test(prevInput) && /[+\-*/.]/.test(value)) {
+      return prevInput.replace(/[+\-*/.]$/, value); // 用新输入的运算符替换掉旧的运算符
+    }
+    
+    // 检查是否按下"="号后直接按下运算符
+    if (prevInput === "" && /[+\-*/.]/.test(value)) {
+      if (result !== "") {
+        return result + value; // 将前一步计算的结果自动拼接到运算符前
+      }
+    }
+    
+    return prevInput + value;
+  });
+};
+
+
 
   const calculateResult = () => {
     try {
-      setResult(eval(input));
+      const validInput = validateInput();
+      setResult(eval(validInput));
       setInput('');
     } catch (error) {
       setResult('Error');
     }
+  };
+
+  const validateInput = () => {
+    let validInput = input;
+    validInput = validInput.replace(/[^0-9+\-*/.]/g, ''); // 移除非数字、加减乘除和小数点的字符
+    validInput = validInput.replace(/([+\-*/.])[+\-*/.]+/g, '$1'); // 连续的运算符只保留第一个
+    validInput = validInput.replace(/^[*/]/, ''); // 去除开头的乘除符号
+    validInput = validInput.replace(/([+\-*/.]$)/, ''); // 去除末尾的运算符和小数点
+    return validInput;
   };
 
   const clearInput = () => {
@@ -29,21 +55,29 @@ const App = () => {
     setResult('');
   };
 
+  const backspace = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+  };
+
   const buttons = [
     ['1', '2', '3', '+'],
     ['4', '5', '6', '-'],
     ['7', '8', '9', '*'],
     ['.', '0', '=', '/'],
-    ['', '', '', 'C'],
+    ['', '', '⌫', 'C'],
   ];
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>{result}</Text>
+        <Text style={styles.resultText} numberOfLines={2}>
+          {result}
+        </Text>
       </View>
       <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>{input}</Text>
+        <Text style={styles.inputText} numberOfLines={2}>
+          {input}
+        </Text>
       </View>
       <View style={styles.buttonsContainer}>
         {buttons.map((row, rowIndex) => (
@@ -60,6 +94,8 @@ const App = () => {
                     clearInput();
                   } else if (button === '=') {
                     calculateResult();
+                  } else if (button === '⌫') {
+                    backspace();
                   } else {
                     handlePress(button);
                   }
@@ -85,13 +121,13 @@ const styles = StyleSheet.create({
     flex: 2,
     justifyContent: 'center',
     alignItems: 'flex-end',
-    paddingRight: 20,
+    padding: 20,
   },
   inputContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'flex-end',
-    paddingRight: 20,
+    padding: 20,
   },
   buttonsContainer: {
     flex: 7,
@@ -119,12 +155,14 @@ const styles = StyleSheet.create({
     color: '#333',
   },
   inputText: {
-    fontSize: 60,
+    fontSize: 40,
     color: '#333',
+    textAlign: 'right',
   },
   resultText: {
-    fontSize: 80,
+    fontSize: 60,
     color: '#333',
+    textAlign: 'right',
   },
 });
 
